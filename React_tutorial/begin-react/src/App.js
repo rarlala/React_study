35e0890cdb02dae26abfefe1f00072d2b75cd389
JEONGSUJI <wtw9611@gmail.com>
2019-12-19 02:53:18 +0900

    react 06.props를 통해 컴포넌트에 값 전달하기

--- React_tutorial/begin-react/src/App.js
+++ React_tutorial/begin-react/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import Hello from './Hello.js';
-import './App.css';
-
-const name = 'react'
-const style = {
-  backgroundColor : 'black',
-  color: 'aqua',
-  fontSize: 24,
-  padding: '1rem'
-}
+import Wrapper from './Wrapper.js';
+// import Wrapper from './Wrapper';
 
 function App() {
   return (
-    <>
-      {/* 주석 작성하는 방법 */}
-      <div style={style}>{name}</div>
-      <Hello 
-      // 열리는 태그 내에서 주석을 작성하는 방법
-      />
-      <div className="gray-box"></div>
-    </>
+    <Wrapper>
+      <Hello name="react" color="red"/>
+      <Hello color="red"/>
+    </Wrapper>
   );
 }
 
