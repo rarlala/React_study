e0d0b405fd8c26bf7e56077b36ace918fd691d24
JEONGSUJI <wtw9611@gmail.com>
2019-12-24 13:35:36 +0900

    06. 조건부 렌더링

--- React_tutorial/begin-react/src/App.js
+++ React_tutorial/begin-react/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Hello from './Hello.js';
 import Wrapper from './Wrapper.js';
-// import Wrapper from './Wrapper';
 
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="red"/>
-      <Hello color="red"/>
+      <Hello name="react" color="red" isSpecial={true}/>
+      <Hello color="orange"/>
     </Wrapper>
   );
 }
--- React_tutorial/begin-react/src/Hello.js
+++ React_tutorial/begin-react/src/Hello.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-function Hello({color, name}) {
-return <div style={{color}}>안녕하세요 {name}</div>;
+function Hello({color, name, isSpecial}) {
+return <div style={{
+  color
+  }}>
+    {/* 삼항연산자를사용하는방법*/}
+    {/* {isSpecial ? <b>*</b> : null} */}
+    
+    {isSpecial && <b>*</b>}
+    {/* AND 연산자를 사용하는 방법 */}
+    안녕하세요 {name}
+  </div>;
 }
 
 Hello.defaultProps = {
